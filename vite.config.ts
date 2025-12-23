4f31bdc7c447b7c4725d86d6679f0a2b26eda700
Matias Zuriel <matiasaquinozc@gmail.com>
2025-12-22 21:32:07 -0500

    feat:changes in vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: 'https://Runniersoaoi.github.io/portfolio-maquino/'
+  base: '/portfolio-maquino/'
 })
